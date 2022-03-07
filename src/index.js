fea37e19810014d2606b5e421bc61aef1d9ca93b
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-03-07 11:55:02 +0100

    Minor update

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to start";
+      const desc = move ? "Go to #" + move : "Go to start";
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
